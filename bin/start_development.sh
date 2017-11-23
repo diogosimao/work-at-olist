6be4ff1649d0370d7bf7d8133302c3ab4b30e966
Diogo Simão <diogosimao@gmail.com>
2017-11-23 21:29:00 -0200

    Adding collect static

--- bin/start_development.sh
+++ bin/start_development.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export DEBUG=True
 export DATABASE_URL=psql://db_user:user_password@127.0.0.1:5432/neattree
+python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
 python manage.py runserver
