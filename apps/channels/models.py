0327602e0683773c97299449972fae53250cc863
Diogo Simão <diogosimao@gmail.com>
2017-11-24 16:33:46 -0200

    Misunderstood of the nature of a ManyToMany relationship

--- apps/channels/models.py
+++ apps/channels/models.py
@@ -5,8 +5,7 @@ from apps.categories.models import Category
 
 
 class Channel(DefaultBaseModel):
-    categories_m2m = models.ManyToManyField(Category, blank=True, related_name='categories_m2m',
-                                            on_delete=models.CASCADE)
+    categories_m2m = models.ManyToManyField(Category, blank=True, related_name='categories_m2m')
 
     def __str__(self):
         return '{}'.format(self.name)
