47e2a6735b2df41a302e8caf10f65fb5b190ea36
Diogo Simão <diogosimao@gmail.com>
2017-11-24 16:06:23 -0200

    Moved some common model fields in order to reuse it

---
+++ neattree/core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from autoslug import AutoSlugField
+
+from .utils import generate_random_string
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-updated_at', '-created_at']
+
+
+class DefaultBaseModel(TimestampedModel):
+    slug = AutoSlugField(db_index=True, max_length=255, unique=True, populate_from='name',
+                         slugify=generate_random_string)
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['slug', 'name']
+
+
