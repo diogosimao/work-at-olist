bd6005bc6bcca032aa360db042f9f708c8e9172d
Diogo Simão <diogosimao@gmail.com>
2017-11-24 16:27:45 -0200

    Added url routes to all views

--- neattree/urls.py
+++ neattree/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+import apps.categories.views
+import apps.channels.views
+
+
 urlpatterns = [
+    url(r'^$', apps.categories.views.index, name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('apps.categories.urls', namespace='categories')),
+    url(r'^api/', include('apps.channels.urls', namespace='channels')),
 ]
